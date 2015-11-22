950048080cc06ae73917ee5a8338a2c3573e11e4
Toni Pohl <info@toni-pohl.de>
2015-11-23 00:06:08 +0100

    Implemented VertexEdgeCount.

--- graphMetrics/src/main/java/de/lwerner/bigdata/graphMetrics/singleJobs/vertexEdgeCount/VertexEdgeCount.java
+++ graphMetrics/src/main/java/de/lwerner/bigdata/graphMetrics/singleJobs/vertexEdgeCount/VertexEdgeCount.java
@@ -1,9 +1,30 @@
 package de.lwerner.bigdata.graphMetrics.singleJobs.vertexEdgeCount;
 
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.graph.Edge;
+import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.Vertex;
+
+import de.lwerner.bigdata.graphMetrics.models.FoodBrokerEdge;
+import de.lwerner.bigdata.graphMetrics.models.FoodBrokerVertex;
+import de.lwerner.bigdata.graphMetrics.utils.FoodBrokerReader;
+
 public class VertexEdgeCount {
 
-	public static void main(String[] args) {
-		// TODO: Not implemented yet
+	public static void main(String[] args) throws Exception {
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		
+		DataSet<Vertex<Long, FoodBrokerVertex>> vertices = FoodBrokerReader.getVertices(env);
+		DataSet<Edge<Long, FoodBrokerEdge>> edges = FoodBrokerReader.getEdges(env);
+		
+		Graph<Long, FoodBrokerVertex, FoodBrokerEdge> graph = Graph.fromDataSet(vertices, edges, env);
+		
+		long verticesCount = graph.numberOfVertices();
+		long edgesCount = graph.numberOfEdges();
+		
+		System.out.println("Number vertices: " + verticesCount);
+		System.out.println("Number edges: " + edgesCount);
 	}
 	
 }
