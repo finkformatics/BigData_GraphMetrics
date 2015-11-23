ac8e6bd17e79412c1997987d9fd12d425d93b169
Toni Pohl <info@toni-pohl.de>
2015-11-23 08:26:02 +0100

    Implemented spread labels.

--- graphMetrics/src/main/java/de/lwerner/bigdata/graphMetrics/singleJobs/spreadLabels/SpreadLabels.java
+++ graphMetrics/src/main/java/de/lwerner/bigdata/graphMetrics/singleJobs/spreadLabels/SpreadLabels.java
@@ -1,9 +1,56 @@
 package de.lwerner.bigdata.graphMetrics.singleJobs.spreadLabels;
 
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.graph.Edge;
+import org.apache.flink.graph.Graph;
+import org.apache.flink.graph.Vertex;
+import org.apache.flink.util.Collector;
+
+import de.lwerner.bigdata.graphMetrics.models.FoodBrokerEdge;
+import de.lwerner.bigdata.graphMetrics.models.FoodBrokerVertex;
+import de.lwerner.bigdata.graphMetrics.utils.FoodBrokerReader;
+
+/**
+ * Job to count the label key of vertices and edges.
+ * 
+ * @author Toni Pohl
+ */
 public class SpreadLabels {
 
-	public static void main(String[] args) {
-		// TODO: Not implemented yet
+	public static void main(String[] args) throws Exception {
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		
+		DataSet<Vertex<Long, FoodBrokerVertex>> vertices = FoodBrokerReader.getVertices(env);
+		DataSet<Edge<Long, FoodBrokerEdge>> edges = FoodBrokerReader.getEdges(env);
+				
+		DataSet<Tuple2<String, Integer>> verticesLabelCount 
+			= vertices.flatMap(new FlatMapFunction<Vertex<Long, FoodBrokerVertex>, Tuple2<String, Integer>>() {
+
+				@Override
+				public void flatMap(Vertex<Long, FoodBrokerVertex> in, Collector<Tuple2<String, Integer>> out)
+						throws Exception {
+					out.collect(new Tuple2<String, Integer>(in.f1.getMeta().get("label").toString(), 1));
+				}
+		})
+		.groupBy(0)
+		.sum(1);
+		
+		DataSet<Tuple2<String, Integer>> edgesLabelCount 
+			= edges.flatMap(new FlatMapFunction<Edge<Long, FoodBrokerEdge>, Tuple2<String, Integer>>() {
+	
+				@Override
+				public void flatMap(Edge<Long, FoodBrokerEdge> in, Collector<Tuple2<String, Integer>> out)
+						throws Exception {
+					out.collect(new Tuple2<String, Integer>(in.f2.getMeta().get("label").toString(), 1));
+				}
+		})
+		.groupBy(0)
+		.sum(1);
+		
+		verticesLabelCount.union(edgesLabelCount).groupBy(0).sum(1).print();
 	}
 	
 }
