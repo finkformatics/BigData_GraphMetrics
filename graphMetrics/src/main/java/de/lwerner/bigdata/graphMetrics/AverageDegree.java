689656ecf420ab43567801ea864b09563b30b192
Toni Pohl <info@toni-pohl.de>
2015-11-30 15:55:59 +0100

    Refactored AverageDegree to extend GraphAlgorithm. Added test case for AverageDegree

--- graphMetrics/src/main/java/de/lwerner/bigdata/graphMetrics/AverageDegree.java
+++ graphMetrics/src/main/java/de/lwerner/bigdata/graphMetrics/AverageDegree.java
@@ -1,13 +1,15 @@
 package de.lwerner.bigdata.graphMetrics;
 
+import org.apache.commons.cli.ParseException;
 import org.apache.flink.api.common.functions.GroupReduceFunction;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.graph.Edge;
-import org.apache.flink.graph.Graph;
 import org.apache.flink.graph.Vertex;
 import org.apache.flink.util.Collector;
+import org.codehaus.jackson.JsonGenerationException;
+import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.node.ObjectNode;
 
@@ -20,13 +22,30 @@ import de.lwerner.bigdata.graphMetrics.utils.GraphMetricsWriter;
 
 import static de.lwerner.bigdata.graphMetrics.utils.GraphMetricsConstants.*;
 
+import java.io.IOException;
+
 /**
  * Calculates the average degree of all vertices
  * 
  * @author Lukas Werner
  * @author Toni Pohl
  */
-public class AverageDegree {
+public class AverageDegree<K extends Number, VV, EV> extends GraphAlgorithm<K, VV, EV> {
+	
+	private double averageDegree = 0;
+
+	public AverageDegree(DataSet<Vertex<K, VV>> vertices, DataSet<Edge<K, EV>> edges, ExecutionEnvironment context) {
+		super(vertices, edges, context);
+	}
+	
+	/**
+	 * Get result of AverageDegree
+	 * 
+	 * @return the average degree
+	 */
+	public double getAverageDegree() {
+		return averageDegree;
+	}
 
 	/**
 	 * Command line arguments
@@ -39,28 +58,24 @@ public class AverageDegree {
 	 * @param args
 	 * @throws Exception
 	 */
-	public static void main(String[] args) throws Exception {
-		arguments = ArgumentsParser.parseArguments(AverageDegree.class.getName(), FILENAME_AVERAGE_DEGREE, args);
+	public static void main(String[] args) {
+		try {
+			arguments = ArgumentsParser.parseArguments(AverageDegree.class.getName(), FILENAME_AVERAGE_DEGREE, args);
+		} catch (IllegalArgumentException | ParseException e1) {
+			e1.printStackTrace();
+			return;
+		}
 		
 		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
 		
 		DataSet<Vertex<Long, FoodBrokerVertex>> vertices = FoodBrokerReader.getVertices(env, arguments.getVerticesPath());
 		DataSet<Edge<Long, FoodBrokerEdge>> edges = FoodBrokerReader.getEdges(env, arguments.getEdgesPath());
 		
-		Graph<Long, FoodBrokerVertex, FoodBrokerEdge> graph = Graph.fromDataSet(vertices, edges, env);
-		
-		// Just need to calculate either out degree or in degree because
-		// every outgoing edge goes to any other vertex as an ingoing edge
-		DataSet<Tuple2<Long, Long>> outDegrees = graph.outDegrees();
-		DataSet<Double> averageDegreeDataSet = outDegrees.reduceGroup(new AverageCalculator());
-		
-		double averageDegree = averageDegreeDataSet.collect().get(0);
-		
-		ObjectMapper m = new ObjectMapper();
-		ObjectNode averageDegreeObject = m.createObjectNode();
-		averageDegreeObject.put("averageDegree", averageDegree);
-		
-		GraphMetricsWriter.writeJson(m, averageDegreeObject, arguments.getOutputPath());
+		try {
+			new AverageDegree<Long, FoodBrokerVertex, FoodBrokerEdge>(vertices, edges, env).runAndWrite();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 	/**
@@ -68,15 +83,15 @@ public class AverageDegree {
 	 * 
 	 * @author Lukas Werner
 	 */
-	private static final class AverageCalculator implements GroupReduceFunction<Tuple2<Long, Long>, Double> {
+	public final class AverageCalculator implements GroupReduceFunction<Tuple2<K, Long>, Double> {
 
 		private static final long serialVersionUID = 1L;
 
 		@Override
-		public void reduce(Iterable<Tuple2<Long, Long>> values, Collector<Double> out) throws Exception {
+		public void reduce(Iterable<Tuple2<K, Long>> values, Collector<Double> out) throws Exception {
 			long sum = 0;
 			long count = 0;
-			for (Tuple2<Long, Long> value: values) {
+			for (Tuple2<K, Long> value: values) {
 				sum += value.f1;
 				count++;
 			}
@@ -84,5 +99,22 @@ public class AverageDegree {
 		}
 		
 	}
-	
+
+	@Override
+	public void run() throws Exception {		
+		// Just need to calculate either out degree or in degree because
+		// every outgoing edge goes to any other vertex as an ingoing edge
+		DataSet<Tuple2<K, Long>> outDegrees = getGraph().outDegrees();
+		DataSet<Double> averageDegreeDataSet = outDegrees.reduceGroup(new AverageCalculator()); 
+		averageDegree = averageDegreeDataSet.collect().get(0);
+	}
+
+	@Override
+	public void writeOutput() throws JsonGenerationException, JsonMappingException, IOException {
+		ObjectMapper m = new ObjectMapper();
+		ObjectNode averageDegreeObject = m.createObjectNode();
+		averageDegreeObject.put("averageDegree", averageDegree);
+		
+		GraphMetricsWriter.writeJson(m, averageDegreeObject, arguments.getOutputPath());	
+	}	
 }
---
+++ graphMetrics/src/test/java/de/lwerner/bigdata/graphMetrics/AverageDegreeTest.java
@@ -0,0 +1,64 @@
+package de.lwerner.bigdata.graphMetrics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.graph.Edge;
+import org.apache.flink.graph.Vertex;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * Test the AverageDegree algorithm
+ * 
+ * @author Toni Pohl
+ */
+public class AverageDegreeTest {
+	AverageDegree<Integer, Integer, Integer> algo;
+	
+	@Before
+	public void setup() {
+		List<Vertex<Integer, Integer>> vs = new ArrayList<>();
+		List<Edge<Integer, Integer>> es = new ArrayList<>();
+
+		for (int i = 1; i < 6; i++) {
+			vs.add(new Vertex<Integer, Integer>(i, i));
+		}
+			
+		/*
+		 * Graph:
+		 *    5 - 1 - 2
+		 *     \  |  /   
+		 *        3
+		 *        |
+		 *        4
+		 */
+		es.add(new Edge<Integer, Integer>(1, 2, 1));
+		es.add(new Edge<Integer, Integer>(2, 1, 2));
+		es.add(new Edge<Integer, Integer>(1, 3, 1));
+		es.add(new Edge<Integer, Integer>(3, 1, 3));
+		es.add(new Edge<Integer, Integer>(1, 5, 1));
+		es.add(new Edge<Integer, Integer>(5, 1, 5));
+		es.add(new Edge<Integer, Integer>(2, 3, 2));
+		es.add(new Edge<Integer, Integer>(3, 2, 3));
+		es.add(new Edge<Integer, Integer>(3, 4, 3));
+		es.add(new Edge<Integer, Integer>(4, 3, 4));
+		es.add(new Edge<Integer, Integer>(3, 5, 3));
+		es.add(new Edge<Integer, Integer>(5, 3, 5));
+		
+		ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+		algo = new AverageDegree<Integer, Integer, Integer>(env.fromCollection(vs), env.fromCollection(es), env);
+	}
+	
+	@Test
+	public void evaluateDegree() {
+		try {
+			algo.run();
+		} catch (Exception e) {
+			Assert.fail("Exception during run: " + e.getMessage());
+		}
+		Assert.assertEquals(2.4, algo.getAverageDegree(), 0.1);
+	}
+}
